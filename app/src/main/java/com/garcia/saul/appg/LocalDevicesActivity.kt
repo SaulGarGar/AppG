1eb36aa4d1efe51234b4dc70ba193b840820ab82
Saul <saul.alejandro.garcia.garcia@gmail.com>
2018-12-23 23:05:12 -0600

    :octocat Create the project.

---
+++ app/src/main/java/com/garcia/saul/appg/LocalDevicesActivity.kt
@@ -0,0 +1,12 @@
+package com.garcia.saul.appg
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class LocalDevicesActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_local_devices)
+    }
+}
